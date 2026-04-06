4ec71b9bcac865faed1deaae80f5ffb819ac9af6
Mohan_mj <dmxmohankumarp@gmail.com>
2026-04-06 09:26:48 +0000

    CountByte

--- FileInputOutputStream/CountByte.java
+++ FileInputOutputStream/CountByte.java
@@ -1,12 +1,14 @@
 import java.io.*;
+import java.util.Scanner;
 class CountByte{
 public static void main(String[] args) throws IOException {
-
+        Scanner scan=new Scanner(System.in);
         FileOutputStream fos=new FileOutputStream("Welcome.txt");
-        String msg="Welcome to the Great Girikalan magic show";
+        String msg;
+        System.out.println("Enter the text to the file:");
+        msg=scan.nextLine();
         fos.write(msg.getBytes());
         fos.close();
-
         FileInputStream fis=new FileInputStream("Welcome.txt");
         int b;
         int count=0;
